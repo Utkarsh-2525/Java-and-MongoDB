a91901ea9c99e04c270a235dc91b210907e6fe42
Utkarsh-2525 <2101641530158@psit.ac.in>
2024-08-11 13:56:34 +0530

    Swagger UI issue

---
+++ JobListing/src/main/java/com/utkarsh2573/joblisting/PostController.java
@@ -0,0 +1,15 @@
+package com.utkarsh2573.joblisting;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class PostController {
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+}
